6bb5707e5fd85cb51478757168cf609751b87be8
DesignMA <designsma.contact@gmail.com>
2025-04-12 20:07:34 -0600

    "Fixing initial practices."

--- Python/Hello.py
+++ Python/Hello.py
@@ -2,14 +2,14 @@ import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
-class MyWidget(QtWidgets.QtWidget): # heredar de la clase QtWidgets.QtWidget
+class MyWidget(QtWidgets.QWidget): # heredar de la clase QtWidgets.QtWidget
 
   def __init__(self): # instancia de widget padre (contenedor de toda la ventana)
     super().__init__() # inicializar clase padre
     self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
     self.button = QtWidgets.QPushButton("Click me!") # instancia de widget boton tipo push
-    self.txt = QtWidgets.Qlabel("Hello World",
-                                alignment = QtCore.QtAlignCenter) # añadir label en el centro
+    self.txt = QtWidgets.QLabel("Hello World",
+                                alignment = QtCore.Qt.AlignCenter) # añadir label en el centro
 
     self.layout = QtWidgets.QVBoxLayout(self) # crear contenedor de tipo caja
     self.layout.addWidget(self.txt) # añadir label al contenedor
@@ -17,9 +17,9 @@ class MyWidget(QtWidgets.QtWidget): # heredar de la clase QtWidgets.QtWidget
 
     self.button.clicked.connect(self.magic) # conectar funcion al ser clickeado
 
-    @QtCore.Slot() # define una funcion externa
-    def magic(self): # recibe el widget padre
-        self.txt.setText(random.choice(self.hello)) # elegir un texto aleatorio al instanciar
+  @QtCore.Slot() # define una funcion externa
+  def magic(self): # recibe el widget padre
+      self.txt.setText(random.choice(self.hello)) # elegir un texto aleatorio al instanciar
 
 
 if __name__ == "__main__": # ejecutar codigo
